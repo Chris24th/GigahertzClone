dc76a8841c0da69d87a4c91f6f2fbf73d6dffdbf
Chris24th <christopherespenida@gmail.com>
2025-04-16 16:01:47 +0800

    fix for deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebook, FaInstagram, FaTiktok, FaYoutube } from 'react-icons';
+import { FaFacebook, FaInstagram, FaTiktok, FaYoutube } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
